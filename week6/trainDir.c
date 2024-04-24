f1296beeaeb964d21d9eda86f3d219654f6f227b
Pavankumar Arakere <pshimoga@gmail.com>
2024-04-24 06:26:44 +0530

    Add files via upload

---
+++ week6/trainDir.c
@@ -0,0 +1,186 @@
+/*flag used to debug on non x86 based CPU */
+//#define SPIKE_DEBUG
+//#define TEST_INPUT
+//#define PRINT_TEST_INPUT
+
+#ifdef SPIKE_DEBUG
+#include <stdio.h>
+#endif /* SPIKE_DEBUG */
+
+/* main function definition */
+void main()
+{
+	/* taking a count to make use of machine cycle */
+	int count = 0;
+	int dbflag;
+	static int key;
+	static int SW1 = 0 , SW2 = 0;
+	int LedPattern = 0;
+	int maskgpio7to0 = 0xFFFFFF00;
+	static int flag = 3;
+	static int read_x30_reg_val;
+	int initLedPattern = 1;
+
+
+	/* infinite loop */
+	while(1)
+	{
+		count++; // increment the count
+		asm volatile(
+				"andi %0, x30, 256\n\t"
+				"srli %0, %0, 8\n\t"		// check with srli
+				"andi %1, x30, 512\n\t"
+				"srli %1, %1, 9\n\t"		// check with srli
+				:"=r"(SW1), "=r"(SW2)
+				:
+				:"x30"
+			    );
+
+#ifdef TEST_INPUT
+		#ifdef PRINT_TEST_INPUT
+		printf("SW1 = %d\tSW2 = %d\n", SW1, SW2);
+		#endif /* PRINT_TEST_INPUT */
+		SW1 = 1;
+#endif /* TEST_INPUT */
+
+		if(SW1 == 1){
+			key = 1;
+		}
+
+		if(SW2 == 1){
+			key = 2;
+		}
+
+		if(SW1 == 1 && SW2 == 1){
+			key = 0;
+		}
+
+#ifdef PRINT_TEST_INPUT
+		printf("SW1 = %d\tSW2 = %d\tkey = %d\n", SW1, SW2, key);
+#endif /* PRINT_TEST_INPUT */
+
+#ifdef PRINT_TEST_INPUT
+		if (count % 49 == 0) 
+			printf(" count = %d\n", count);
+#endif /*PRINT_TEST_INPUT*/
+
+		if(count == 50 && dbflag == 0 && key)
+		{
+			/* calling a function train flow */
+			dbflag = 1;
+			if(flag == 1)
+			{
+				LedPattern = LedPattern & 0x0000007F;
+				LedPattern <<= 1;
+				asm volatile(
+						"and x30, x30, %0\n\t"
+						"or x30, x30, %1\n\t"
+						:
+						:"r"(maskgpio7to0), "r"(LedPattern)
+						:"x30"
+					    );
+#ifdef SPIKE_DEBUG
+				asm volatile(
+						"andi %0, x30, 255\n\t"
+						:"=r"(read_x30_reg_val)
+						:
+						:"x30"
+					    );
+#endif /* SPIKE_DEBUG */
+
+				if(key == 2)
+					flag = 4;
+				if(LedPattern == 0x00)
+					flag = 2;
+			}
+			else if(flag == 2)
+			{
+				if(key == 1 && initLedPattern == 1){
+					initLedPattern = 0;
+					LedPattern = 0;
+				}
+
+				LedPattern = (LedPattern <<= 1) | 1;
+				asm volatile(
+						"and x30, x30, %0\n\t"
+						"or x30, x30, %1\n\t"
+						:
+						:"r"(maskgpio7to0), "r"(LedPattern)
+						:"x30"
+					    );
+#ifdef SPIKE_DEBUG
+				asm volatile(
+						"andi %0, x30, 255\n\t"
+						:"=r"(read_x30_reg_val)
+						:
+						:"x30"
+					    );
+#endif /* SPIKE_DEBUG */
+
+				if(key == 2)
+					flag = 3;
+				if(LedPattern == 0xFF)
+					flag = 1;
+			}
+			else if(flag == 3)
+			{
+				if(key == 1){
+					goto case2;
+				}
+				LedPattern = (LedPattern >>= 1) | 0X80;
+				asm volatile(
+						"and x30, x30, %0\n\t"
+						"or x30, x30, %1\n\t"
+						:
+						:"r"(maskgpio7to0), "r"(LedPattern)
+						:"x30"
+					    );
+#ifdef SPIKE_DEBUG
+				asm volatile(
+						"andi %0, x30, 255\n\t"
+						:"=r"(read_x30_reg_val)
+						:
+						:"x30"
+					    );
+#endif /* SPIKE_DEBUG */
+			case2:
+				if(key == 1)
+					flag = 2;
+				if(LedPattern == 0xFF)
+					flag = 4;
+			}
+			else if(flag == 4)
+			{
+				LedPattern >>= 1;
+				asm volatile(
+						"and x30, x30, %0\n\t"
+						"or x30, x30, %1\n\t"
+						:
+						:"r"(maskgpio7to0), "r"(LedPattern)
+						:"x30"
+					    );
+#ifdef SPIKE_DEBUG
+				asm volatile(
+						"andi %0, x30, 255\n\t"
+						:"=r"(read_x30_reg_val)
+						:
+						:"x30"
+					    );
+#endif /* SPIKE_DEBUG */
+
+				if(key == 1)
+					flag = 1;
+				if(LedPattern == 0x00)
+					flag = 3;
+			}
+#ifdef SPIKE_DEBUG
+			printf("key = %d\t\t flag = %d\t\tLedPattern = %d\t\t\tread_x30_reg_val = %d\n", key, flag, LedPattern, read_x30_reg_val);
+#endif
+			count = 0;// count initialize with zero 
+		}
+
+		if(count == 10)
+			dbflag = 0;
+	} /*infinite loop*/
+}
+
